1f28eab40c7c24b264d325c092ff95cf5fc4a257
Nate Sire <nate.sire@fairwaymc.com>
2022-06-09 18:20:16 -0400

    write factorialize function

--- source/anagram.ts
+++ source/anagram.ts
@@ -1,9 +1,45 @@
 export class Anagram {
-    constructor(public dictionaryFile: string) { }
+  constructor(public dictionaryFile: string) { }
 
-    find(word: string): string {
-        if(word === 'finder') { return 'friend'; }
-        if(word === 'friend') { return 'finder'; }
-        return '';
+  find(word: string): string {
+      if(word === 'finder') { return 'friend'; }
+      if(word === 'friend') { return 'finder'; }
+      return '';
+  }
+
+  // Time Complexity: O(?)
+  // Space Complexity: O(?)
+  permutatePossibleAnagrams(word: string): string {
+    let permutations: string[] = [];
+    let possibleAnagrams: string[] = [];
+    let letters: string[] = word.split('');
+
+    // max permutations n = factorial of n digits
+    let maxPermutations = this.factorialize(letters.length);
+
+    for(let i = 0; i < maxPermutations; i++) {
+      return this.swap(i, i+1, word);
+    };
+  }
+
+  swap(i:number, j:number, word:string) {
+    let letters = word.split('');
+    let charA = letters[i];
+    let charB = letters[j];
+    letters[i] = charB;
+    letters[j] = charA;
+    //join the letters back together
+    return letters.join('');
+  }
+
+  // move to another class
+  factorialize(num) {
+    let factorialized = num;
+    if (num === 0 || num === 1) return 1; 
+    while (num > 1) { 
+      num--;
+      factorialized = factorialized * num;
     }
+    return factorialized;
+  }
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,13 +1,18 @@
 import { Anagram } from '../source/anagram.js';
 
 describe("should find anagram of", () => {
-    it('finder -> friend', () => {
-        let anagram = new Anagram('dictionary.txt');
-        expect(anagram.find('finder')).toContain('friend');
+    it('ifnder -> finder', () => {
+      let anagram = new Anagram('dictionary.txt');
+      expect(anagram.find('friend')).toContain('finder');
     });
 
-    it('friend -> finder', () => {
-        let anagram = new Anagram('dictionary.txt');
-        expect(anagram.find('friend')).toContain('finder');
+    it('should factorialize 5 to 120', () => {
+      let anagram = new Anagram('dictionary.txt');
+      expect(anagram.factorialize(5)).toBe(120);
+    });
+
+    it('should return first permutation', () => {
+      let anagram = new Anagram('dictionary.txt');
+      expect(anagram.swap(0, 1, 'friend')).toContain('rfiend');
     });
 });
