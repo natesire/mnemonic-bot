ed36fcde3e7bba5c7d39870b0e03570639c076fb
Nate Sire <nate.sire@fairwaymc.com>
2022-06-10 18:24:54 -0400

    added comma as delimeter

--- source/anagram.ts
+++ source/anagram.ts
@@ -23,12 +23,22 @@ export class Anagram {
 
   // array passed by reference
   iterateDictionaryAndSort(dictionary: string[]) : Map<string, string> {
+    let delimeter: string = '';
+    let i = 0;
     dictionary.forEach(word => {
       let preVal = ' ' + this.sortedDictionary.get(this.sortWord(word)); // check if pre-existing key
       if(preVal === ' ' + undefined) { preVal = ''; }
-      let sortedWord = this.sortWord(word)
-      //if(!preVal.split('').includes(sortedWord)) { preVal += ' ' + sortedWord; }
-      this.sortedDictionary.set(sortedWord, word + preVal);
+      let sortedWord = this.sortWord(word);
+      let newVal = preVal + delimeter + word;
+      //strip off the first comma
+      if(newVal.charAt(0) === ',') { newVal = newVal.substring(1); }
+      if(newVal.charAt(0) === ' ') { newVal = newVal.substring(1); }
+
+      this.sortedDictionary.set(sortedWord, newVal);
+      delimeter = ',';
+      //console.log(i)
+      //console.log(`preVal: ${preVal}`);
+      i++;
     });
     return this.sortedDictionary;
   }
