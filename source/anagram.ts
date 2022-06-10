bf76f6d0501b8701c8bc125f03293829bb4f6f88
Nate Sire <nate.sire@fairwaymc.com>
2022-06-10 17:54:19 -0400

    sorting solution is working

--- source/anagram.ts
+++ source/anagram.ts
@@ -3,25 +3,34 @@ import fs from 'fs';
 // find all anagrams in dictionary
 export class Anagram {
   public dictionary: string[] = []; 
+  public sortedDictionary: Map<string, string>;
   
   constructor(public dictionaryFile: string) {
     this.dictionaryFile = dictionaryFile;
+
+    this.sortedDictionary = new Map(); // hash map
+
+    // validations here to keep the code clean
+    if (!fs.existsSync(this.dictionaryFile)) {
+      throw new Error('File not found!');
+    }
   }
 
   loadDictionaryIntoArray(): string[] {
-    var dictionary: string[] = [];
-    try {
-      var dictionary: string[] = fs.readFileSync(this.dictionaryFile, 'utf8').split('\n');
-    } catch (error) {
-      console.log(error);
-    }
+    var dictionary: string[] = fs.readFileSync(this.dictionaryFile, 'utf8').split('\r\n');
     return dictionary;
   }
 
   // array passed by reference
-  iterateDictionary(dictionary: string[]) : string[] {
-    dictionary.map(word => { this.sortWord(word); });
-    return dictionary;
+  iterateDictionaryAndSort(dictionary: string[]) : Map<string, string> {
+    dictionary.forEach(word => {
+      let preVal = ' ' + this.sortedDictionary.get(this.sortWord(word)); // check if pre-existing key
+      if(preVal === ' ' + undefined) { preVal = ''; }
+      let sortedWord = this.sortWord(word)
+      //if(!preVal.split('').includes(sortedWord)) { preVal += ' ' + sortedWord; }
+      this.sortedDictionary.set(sortedWord, word + preVal);
+    });
+    return this.sortedDictionary;
   }
 
   sortWord(word: string) {
--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+import { Anagram } from '../dist/source/anagram.js';
 import readline from 'readline';
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,5 +10,14 @@ const rl = readline.createInterface({
 //read user input
 rl.question('enter word to find anagrams:', function (word) {
   console.log(`anagrams of ${word}:`);
+
+  // match this
+  let sortedWord = word.split('').sort().join('');
+
+  let anagram = new Anagram('../dictionary3.txt');
+  const dictionary = anagram.loadDictionaryIntoArray();
+  let sortedDictionary = anagram.iterateDictionaryAndSort(dictionary);
+  let anagrams = sortedDictionary.get(sortedWord)
+  console.log(anagrams);
   rl.close();
 });
