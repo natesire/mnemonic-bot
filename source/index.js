23f2e403c2c95aa2d7055f4c329a1e8ff0969238
Nate Sire <nate.sire@fairwaymc.com>
2022-06-12 18:41:29 -0400

    add exit command to command line

--- source/index.js
+++ source/index.js
@@ -7,16 +7,21 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-//read user input
-rl.question('enter word to find anagrams:', function (word) {
-  console.log(`anagrams of ${word}:`);
+var waitForUserInput = function() {
+  rl.question("enter word to find anagrams: ", function(word) {
+    let anagram = new Anagram('../dictionary.test.txt');
+    let anagrams = anagram.findAnagrams(word);
 
-  let anagram = new Anagram('../dictionary.test.txt');
-  let anagrams = anagram.findAnagrams(word);
+    // return anagrams if found
+    if(anagrams) console.log(anagrams);
+    if(anagrams === undefined) console.log(`No anagrams found for ${word}`);
+    
+    if (word == "exit"){
+        rl.close();
+    } else {
+        waitForUserInput();
+    }
+  });
+}
 
-  // return anagrams if found
-  if(anagrams) console.log(anagrams);
-  if(anagrams === undefined) console.log(`No anagrams found for ${word}`);
-  
-  rl.close(); // close the readline interface
-});
+waitForUserInput();
