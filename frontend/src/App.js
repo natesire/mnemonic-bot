cbe32b30e7e5615c93854e594a8d53b144633a43
Nate Sire <nate.sire@fairwaymc.com>
2023-10-09 00:32:21 -0400

    writing search function. need an api server endpoint.

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Enter a word to create Anagrams
+        </p>
+        <input id='search'></input>
+        <input type='submit' value='Generate' onClick={search}></input>
+      </header>
+    </div>
+  );
+}
+
+function search() {
+  var word = document.getElementById('search').value;
+  alert('search')
+  /*
+  fetch('http://localhost:5000/anagrams/' + word)
+    .then(response => response.json())
+    .then(data => {
+      var anagrams = data.anagrams;
+      var anagramString = '';
+      for (var i = 0; i < anagrams.length; i++) {
+        anagramString += anagrams[i] + ', ';
+      }
+      alert(anagramString);
+    });
+    */
+}
+
+export default App;
