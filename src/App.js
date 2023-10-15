44cecec5be17880573854953e7db228841473fea
Nate Sire <nate.sire@fairwaymc.com>
2023-10-15 02:51:47 -0400

    custom image from Dalle2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+<div className="container">
+  <img className="typewriter" src="typewriter2.png" alt="typewriter"></img>
+</div>
+
         <p>
           Search for Anagrams
         </p>
