bfd95fde7c8f4da4219c4eb07151481141fabe97
Nate Sire <nate.sire@fairwaymc.com>
2023-10-21 03:26:29 -0400

    error: 'Fetch.spec.ts' cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.

--- anagram2/src/App.tsx
+++ anagram2/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <input type="text" id="input1" placeholder="Enter first word" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
